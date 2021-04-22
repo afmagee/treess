f2a1bfde3ea49c88c6f015b50ad21f9348edab14
Andrew Magee <afmagee@uw.edu>
2021-04-22 15:42:28 -0700

    Moving to a hacked version of RWTY's approximateESS

--- R/approximateESS.R
+++ R/approximateESS.R
@@ -6,12 +6,13 @@
 #' @param alpha The cutoff proportion of the asymptoted used to estimate the average pairwise squared distance.
 #' @param min.nsamples We only compute the average squared distance between x_t and x_{t+T} if there are at least this many samples.
 #' @keywords internal
-approximateESS <- function(dmat,trees=NA,nsim=NA,min.nsamples=5,alpha=0.05) {
-  # recover()
+approximateESS <- function(dmat,trees=NA,nsim=NA,min.nsamples=(dim(dmat)[1] - 100),alpha=0.05) {
+  # This function essentially strings together the following RWTY functions
+  # topological.autocorr, topological.approx.ess, approx.ess.multi, approx.ess.single, tree.autocorr
   
   dmat <- dmat^2
   
-  n <- dim(dmat)[1]
+  N <- n <- dim(dmat)[1]
   
   # alpha to threshold
   thresh = 1 - alpha
@@ -20,6 +21,7 @@ approximateESS <- function(dmat,trees=NA,nsim=NA,min.nsamples=5,alpha=0.05) {
   max_off_diag <- n - min.nsamples
   
   # get empirical curve of lag time t vs distance
+  # see also topological.autocorr
   t <- 1:max_off_diag
   d_t <- sapply(t,function(t_){
     mean(dmat[row(dmat) == col(dmat) + t_])
@@ -31,25 +33,27 @@ approximateESS <- function(dmat,trees=NA,nsim=NA,min.nsamples=5,alpha=0.05) {
     return( sum((E_d - d_t)^2) )
   }
   par <- optim(c(1,1),fn,t=t,d_t=d_t)
-  
+    
   # For finding the cutoff lag
   thresh <- thresh * par$par[1]
-
-  E_delta_squared_iid <- 0
   
-  if ( max(d_t) < thresh ) {
-    E_delta_squared_iid <- max(dmat)
-  } else {
-    thin <- min(which(d_t >= thresh))
-    E_delta_squared_iid <- sum(unlist(lapply(thin:n,function(i){
-      dmat[row(dmat) == col(dmat) + i]
-    })))/choose(n-thin,2)
+  m <- ifelse( max(d_t) < thresh, length(d_t)+1, min(which(d_t >= thresh)))
+  
+  # The below code is from approx.ess.single, with d_t in place of df$topo.distance 
+  D <- max(d_t)
+  
+  S = 0
+  
+  if(m>1){
+    for(k in 1:(m - 1)){
+      f = d_t[k]
+      S = S + ((N - k) * f)
+    }
   }
   
-  # Solve for ESS 
-  rhs <- sum(dmat)/(n^2)
+  S = S + (N - m + 1) * (N - m) * D / 2
+  S = S / 2 / N^2
+  ESS = 1 / (1 - 4 * S / D)
   
-  ess <- 1/(1 - rhs/E_delta_squared_iid)
-  
-  return(ess)
+  return(ESS)
 }
