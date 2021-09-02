4c134add67496f678f0d47e6a4f837b5db886a1a
Andrew Magee <afmagee@uw.edu>
2021-09-02 10:30:41 -0700

    Defensive coding for unstable MDS

--- R/CMDSESS.R
+++ R/CMDSESS.R
@@ -4,6 +4,11 @@
 #' @keywords internal
 CMDSESS <- function(dmat,...) {
   dmat <- dmat^2
+  # cmdscale does not work well if all samples are the same
+  # So, we pre-empt this and declare the ESS to be 1 (there's 1 distinct value)
+  if ( all(dmat == 0) ) {
+    return(1.0)
+  }
   x <- stats::cmdscale(dmat,k=1)[,1]
   return(coda::effectiveSize(x))
 }
